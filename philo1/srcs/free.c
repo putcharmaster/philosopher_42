4f7bd099818fab99b4eda65cedf0f05cc3987b52
sangha <putcharmaster@gmail.com>
2024-11-22 21:56:19 +0100

    cleaned the repo. phil2 is the right one

--- philo1/srcs/free.c
+++ philo1/srcs/free.c
@@ -5,17 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanhwang <sanhwang@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 20:20:08 by sanhwang          #+#    #+#             */
-/*   Updated: 2024/11/21 22:38:37 by sanhwang         ###   ########.fr       */
+/*   Created: 2024/11/10 20:20:08 by sanhwang          #+#    #+#             */
+/*   Updated: 2024/11/22 21:37:35 by sanhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+
+void	clean_exit(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < data->num_philos)
+		pthread_mutex_destroy(&data->forks[i]);
+	pthread_mutex_destroy(&data->write_mutex);
+	pthread_mutex_destroy(&data->death_mutex);
+	pthread_mutex_destroy(&data->meal_mutex);
+	free(data->forks);
+	free(data->philos);
+}
+
 void	free_all(t_data *data)
 {
-    int i;
-    i = 0;
+	int	i;
+
+	i = 0;
 	while (i < data->args.num_philos)
 		pthread_mutex_destroy(&data->forks[i++]);
 	pthread_mutex_destroy(&data->print_lock);
